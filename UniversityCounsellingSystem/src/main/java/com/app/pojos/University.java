f376234f81f66eb75fd740d76748e73a3754e31e
shantanumali <shantumali95@gmail.com>
2022-03-30 18:14:07 +0530

    Added a few new annotations

--- UniversityCounsellingSystem/src/main/java/com/app/pojos/University.java
+++ UniversityCounsellingSystem/src/main/java/com/app/pojos/University.java
@@ -3,10 +3,12 @@ package com.app.pojos;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name="university_tbl")
 public class University extends BaseEntity{
 	@Column(name="university_name", unique=true)
+	@NotBlank(message = "University name can't be blank")
 	private String universityName;
 }
--- UniversityCounsellingSystem/src/main/java/com/app/pojos/User.java
+++ UniversityCounsellingSystem/src/main/java/com/app/pojos/User.java
@@ -11,7 +11,9 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,13 +29,16 @@ import javax.persistence.Enumerated;
 @Data
 //name | email_id | password | role | phone_no
 public class User extends BaseEntity {
+	@NotBlank(message = "Name must be supplied")
 	private String name;
 	@Column(name = "email_id", unique = true) // adds unique constraint
+	@NotBlank(message = "email can't be blank")
 	@Email(message = "Email is not valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
 	// Regex for email validation
 	@NotEmpty(message = "Email cannot be empty")
 	private String emailId;
 	@Column(nullable = false) // varchar(30), NOT NULL constraint
+	@Pattern(regexp="((?=.*\\d)(?=.*[a-z])(?=.*[#@$*]).{5,20})",message = "Invalid password")
 	private String password;
 	@Enumerated(EnumType.STRING) // => column : varchar
 	private Role role;
