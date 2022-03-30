5497246cbf9442fbd9f55e6b97429bc2b4d64aaf
shantanumali <shantumali95@gmail.com>
2022-03-30 14:41:06 +0530

    Added User, University and Role

---
+++ UniversityCounsellingSystem/src/main/java/com/app/pojos/University.java
@@ -0,0 +1,12 @@
+package com.app.pojos;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="university_tbl")
+public class University extends BaseEntity{
+	@Column(name="university_name")
+	private String universityName;
+}
---
+++ UniversityCounsellingSystem/src/main/java/com/app/pojos/User.java
@@ -0,0 +1,42 @@
+package com.app.pojos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.JoinColumn;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Entity
+@Table(name = "users_tbl")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+//name | email_id | password | role | phone_no
+public class User extends BaseEntity {
+	private String name;
+	@Column(name = "email_id", unique = true) // adds unique constraint
+	@Email(message = "Email is not valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+	// Regex for email validation
+	@NotEmpty(message = "Email cannot be empty")
+	private String emailId;
+	@Column(nullable = false) // varchar(30), NOT NULL constraint
+	private String password;
+	@Enumerated(EnumType.STRING) // => column : varchar
+	private Role role;
+	@Column(name = "phone_no")
+	private int phoneNo;
+}
