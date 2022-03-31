9d3813611512686392e2f0265539bab3def1df2f
shantanumali <shantumali95@gmail.com>
2022-03-31 11:27:56 +0530

    Added application properties in inner .gitignore

--- UniversityCounsellingSystem/src/main/java/com/app/pojos/University.java
+++ UniversityCounsellingSystem/src/main/java/com/app/pojos/University.java
@@ -3,6 +3,7 @@ package com.app.pojos;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Entity
@@ -11,4 +12,10 @@ public class University extends BaseEntity{
 	@Column(name="university_name", unique=true)
 	@NotBlank(message = "University name can't be blank")
 	private String universityName;
+	
+	@Column(name = "email_id", unique = true) // adds unique constraint
+	@NotBlank(message = "email can't be blank")
+	@Email(message = "Email is not valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+	// Regex for email validation
+	private String emailId;
 }
