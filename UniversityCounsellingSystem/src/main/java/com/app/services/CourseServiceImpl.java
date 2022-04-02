286fcf9ecee2a0a41401b13c69b723b97e0cd3aa
aman-chairasia <aman-chairasia@gmail.com>
2022-04-02 10:51:35 +0530

    aman added home controller

---
+++ UniversityCounsellingSystem/src/main/java/com/app/services/CourseServiceImpl.java
@@ -0,0 +1,30 @@
+package com.app.services;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.dao.CoureseRepository;
+import com.app.pojos.Course;
+
+@Service
+@Transactional
+public class CourseServiceImpl implements ICourseService {
+	
+	@Autowired
+	private CoureseRepository courseRepo;
+
+	@Override
+	public List<Course> listOfCourses() {
+		// TODO Auto-generated method stub
+		return courseRepo.findAll();
+	}
+	
+	
+	
+	
+
+}
