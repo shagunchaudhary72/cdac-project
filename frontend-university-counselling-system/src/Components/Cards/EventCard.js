0e88acbae4825a46c9ba996ac80fc11febda7d00
Shagun Chaudhary <chshagun72@gmail.com>
2022-04-14 20:26:10 +0530

    events added

--- frontend-university-counselling-system/src/Components/Cards/EventCard.js
+++ frontend-university-counselling-system/src/Components/Cards/EventCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const EventCard = ({eventDetails}) => {
+const EventCard = ({eventDetails, resultDate, updationDate}) => {
   return (
-    <div className="card w-85 my-3">
+    <div className="card w-85 my-3 event-card">
       <div className="card-body">
-        <h5 className="card-title">{eventDetails.title}</h5>
+        <h5 className="card-title border-bottom border-success">{eventDetails.title} </h5>
         <p className="card-text">
-          {eventDetails.news}
+          {eventDetails.news} <span className="text-success fst-italic">{ eventDetails.id === 1 ? resultDate : updationDate }</span>
         </p>
       </div>
     </div>
--- frontend-university-counselling-system/src/Components/Pages/EventsPage.js
+++ frontend-university-counselling-system/src/Components/Pages/EventsPage.js
@@ -1,18 +1,35 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import events from '../../EventsData';
 import EventCard from '../Cards/EventCard';
+import AdminService from '../../Services/AdminService';
 
 const EventsPage = () => {
 
-    const [eventsData, SetEventsData] = useState(events);
+  const [ resultDate, setResultDate] = useState();
+  const [ updationDate, setUpdationDate] = useState();
+  const [eventsData, SetEventsData] = useState(events);
 
+    useEffect(() => {
+      AdminService.getAcademicDates().then((response) => {
+        if( response.status === 200 ){
+            setResultDate( response.data.resultDate );
+            setUpdationDate( response.data.updationDate );
+            SetEventsData(events);
+            console.log( response.data );
+        }
+    }).catch((err) => {
+        console.log(err);
+    });
+    console.log(events);
+    },[])
+    
   return (
     <div className='event-page my-5'>
         <h1 className='text-center my-3'>Upcoming Events</h1>
         <div className='event-card w-50 mx-auto'>
-          {eventsData.map((event) => {
-            return <EventCard key={event.id} event={event} />;
+          { eventsData.length > 0 && eventsData.map((event) => {
+            return <EventCard key={event.id} eventDetails={event} resultDate={resultDate} updationDate={updationDate}/>;
           }
           )}
           </div>
